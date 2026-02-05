574c73af0c9a70ccaceab785b37cfbc2225cb9a0
Javier Jarp <javi.jarp1@gmail.com>
2026-02-05 16:40:38 -0400

    edge-module branch: keep only edge-module directory

--- backend/src/main/java/com/unibe/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unibe.repository;
-
-import com.unibe.model.DetectionEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRepository extends JpaRepository<DetectionEvent, Long> {
-}
