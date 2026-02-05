e65db5b18d15e179f7cc5357d98c35d25df97837
Javier Jarp <javi.jarp1@gmail.com>
2026-02-05 17:40:58 -0400

    Springboot proyect

---
+++ backend/security-backend/src/main/java/com/javier/security_backend/SecurityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.javier.security_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBackendApplication.class, args);
+	}
+
+}
